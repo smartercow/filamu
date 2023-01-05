674cf16465729a7fbec9dae8d8af4779d9b5fcb7
smartercow <smartercow@gmail.com>
2023-01-05 01:42:27 +0100

    update

--- src/utils/admin/stripe.ts
+++ src/utils/admin/stripe.ts
@@ -1,15 +1,13 @@
 import Stripe from "stripe";
 
-export const stripe = new Stripe(
-  (process.env.STRIPE_SECRET_KEY as string) || "",
-  {
-    // https://github.com/stripe/stripe-node#configuration
-    apiVersion: "2022-08-01",
-    // Register this as an official Stripe plugin.
-    // https://stripe.com/docs/building-plugins#setappinfo
-    appInfo: {
-      name: `Filamu`,
-      version: "0.1.0",
-    },
-  }
-);
+const stripeSecretKey = process.env.STRIPE_SECRET_KEY as string;
+export const stripe = new Stripe(stripeSecretKey, {
+  // https://github.com/stripe/stripe-node#configuration
+  apiVersion: "2022-08-01",
+  // Register this as an official Stripe plugin.
+  // https://stripe.com/docs/building-plugins#setappinfo
+  appInfo: {
+    name: `Filamu`,
+    version: "0.1.0",
+  },
+});
--- src/utils/admin/supabase-admin-client.ts
+++ src/utils/admin/supabase-admin-client.ts
@@ -1,7 +1,10 @@
 import { createClient } from "@supabase/supabase-js";
 import { Database } from "@/types/supabase-types";
 
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
+const supabaseServiceRole = process.env.SUPABASE_SERVICE_ROLE_KEY as string;
+
 export const supabaseAdmin = createClient<Database>(
-  (process.env.NEXT_PUBLIC_SUPABASE_URL as string) || "",
-  (process.env.SUPABASE_SERVICE_ROLE_KEY as string) || ""
+  supabaseUrl,
+  supabaseServiceRole
 );
