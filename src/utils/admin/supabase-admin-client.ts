ff068db1e8fc71f52f349cac930b43bebe8820b4
smartercow <smartercow@gmail.com>
2023-01-05 01:46:50 +0100

    update

--- src/utils/admin/supabase-admin-client.ts
+++ src/utils/admin/supabase-admin-client.ts
@@ -1,8 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
 import { Database } from "@/types/supabase-types";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
-const supabaseServiceRole = process.env.SUPABASE_SERVICE_ROLE_KEY as string;
+const supabaseUrl = (process.env.NEXT_PUBLIC_SUPABASE_URL as string) || "";
+const supabaseServiceRole =
+  (process.env.SUPABASE_SERVICE_ROLE_KEY as string) || "";
 
 export const supabaseAdmin = createClient<Database>(
   supabaseUrl,
