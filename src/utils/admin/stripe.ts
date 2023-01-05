ce5c57466631f1182f9ed151608e8dc3742177be
smartercow <smartercow@gmail.com>
2023-01-05 01:13:06 +0100

    update

--- src/utils/admin/stripe.ts
+++ src/utils/admin/stripe.ts
@@ -1,6 +1,6 @@
 import Stripe from "stripe";
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || "", {
   // https://github.com/stripe/stripe-node#configuration
   apiVersion: "2022-08-01",
   // Register this as an official Stripe plugin.
